4cf981ebd3682d27372ca06e568893f3176f8846
Altarium <31167322+AltariasMainAccount@users.noreply.github.com>
2021-08-04 12:47:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+const mr = require ('./structure/menuRender.js');
+const mls = require ('./structure/multi_line_strings.js');
+const mfa = require ('./structure/menu_function_arrays.js');
+
+readline.emitKeypressEvents(process.stdin);
+console.clear();
+process.stdin.setRawMode(true);
+console.clear();
+let menu1 = mr.createMenu(mls.menu2);
+console.clear();
+
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+        process.exit();
+    } else {
+        mr.processKey(key, menu1, mfa.menu1);
+    }
+});
---
+++ structure/multi_line_strings.js
@@ -0,0 +1,16 @@
+module.exports = {
+menu1: `
+First Option
+Second Option
+Third Option
+Fourth Option
+Terminate Program    
+`,
+menu2: `
+Print some garbage
+Print some garbage
+Print some garbage
+Print some garbage
+Exit lol
+`
+}
